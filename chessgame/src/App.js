e97dd82d3c8a784566f12acb5b52269419de3dfe
bp289 <birajpanta112@gamil.com>
2023-02-03 16:42:59 +0000

    chessboard

---
+++ chessgame/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Chessboard from "./components/Chessboard.js";
+function App() {
+  return (
+    <div className="App">
+      <Chessboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ chessgame/src/components/Chessboard.js
@@ -0,0 +1,16 @@
+import "./Chessboard.css";
+const xAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+export default function Chessboard() {
+  let board = [];
+
+  for (let x = 1; x <= 8; x++) {
+    for (let y = 8; y >= 1; y--) {
+      const tileClass = (x + y) % 2 === 0 ? "light" : "dark";
+      board.push(
+        <span className={tileClass} key={`${xAxis[x - 1]}${y}`}></span>
+      );
+    }
+  }
+  return <div className="board">{board}</div>;
+}
