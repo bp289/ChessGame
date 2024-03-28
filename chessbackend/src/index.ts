33778eebc704ad43354b56b16c2315dd04613752
bp <birajpanta112@gmail.com>
2024-03-28 00:27:34 +0000

    websocket implemented

--- chessbackend/src/index.ts
+++ chessbackend/src/index.ts
@@ -2,20 +2,28 @@ import express, { Express, Request, Response } from "express";
 
 import dotenv from "dotenv";
 import expressWs from "express-ws";
-import { router as chessGameRouter } from "./webSockets/chessGames";
-
-dotenv.config();
-
-const { app, getWss, applyTo } = expressWs(express());
+import {
+  router as chessGameRouter,
+  mountChessWs,
+} from "./webSockets/chessGames";
 
 const port = process.env.PORT || 3000;
+dotenv.config();
 
-app.use("/ws-stuff", chessGameRouter);
+const createApp = () => {
+  const { app, getWss, applyTo } = expressWs(express());
+  mountChessWs();
+  app.use("/ws-stuff", chessGameRouter);
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Home Server");
+  });
+  return app;
+};
+const main = () => {
+  const app = createApp();
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+};
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Home Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+main();
--- chessbackend/src/webSockets/chessGames.ts
+++ chessbackend/src/webSockets/chessGames.ts
@@ -1,9 +1,46 @@
 import express, { Express, Request, Response, Router } from "express";
-import expressWs from "express-ws";
-export const router = express.Router() as expressWs.Router;
+import expressWs, { WebsocketMethod } from "express-ws";
+import webSocket from "ws";
 
-router.ws("/echo", (ws, req) => {
-  ws.on("message", function (msg) {
-    ws.send(msg);
+interface Rooms {
+  [key: string]: webSocket[];
+}
+export const router = express.Router();
+
+const rooms: Rooms = {};
+
+const broadCast = (roomId: string, message: string, ws: webSocket) => {
+  rooms[roomId].forEach((client: webSocket) => {
+    if (client != ws && client.readyState === client.OPEN) {
+      client.send(message);
+    }
   });
-});
+};
+
+export const mountChessWs = () => {
+  router.ws("/game", (ws: webSocket, req: Request) => {
+    console.log("socket", req.query);
+
+    const roomId = req.query.roomId! as string;
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+
+    console.log(`New WebSocket connection to room "${roomId}"`);
+    rooms[roomId].push(ws);
+
+    ws.on("message", function (msg: string) {
+      console.log(`said:${msg}`);
+      broadCast(roomId, `you said:${msg}`, ws);
+    });
+
+    ws.on("close", () => {
+      const index: number = rooms[roomId].indexOf(ws);
+      if (index !== -1) {
+        rooms[roomId].splice(index, 1);
+        console.log(`Connection closed in room "${roomId}"`);
+      }
+    });
+  });
+};
